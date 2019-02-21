d1b0051c18b0496347e81fe61b63066009510c7b
parkercrandell <thatoneguy1c@gmail.com>
2019-02-21 11:47:03 -0500

    importing everything

---
+++ Autobio Game/Assets/CameraControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Rigidbody thisRigidBody;
+    public Camera thisCamera;   // the camera
+
+    public float pitch; // the mouse movement up/down
+    public float yaw;   // the mouse movement left/right
+
+
+    public Vector3 inputVelocity;  // cumulative velocity to move character
+
+    public float velocityModifier;  // velocity of conroller multiplied by this number
+
+    void Start()
+    {
+        thisRigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+        yaw = Input.GetAxis("Mouse X");
+        transform.Rotate(0, yaw, 0);
+
+        pitch = Input.GetAxis("Mouse Y");
+        thisCamera.transform.Rotate(-pitch, 0, 0);
+
+    }
+}
+
---
+++ Autobio Game/Assets/sphereMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphereMovement : MonoBehaviour
+{
+    public Rigidbody thisRigidBody;
+    public Vector2 inputAxis;
+    public float moveVelocity;
+    public float forceMultiplier;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
