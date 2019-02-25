67c09f38428839114564b7a2ee389d5b4970860d
parkercrandell <thatoneguy1c@gmail.com>
2019-02-25 01:18:33 -0500

    Camera Animation, Player movement, Collision WIP ( PUSH COULD BE BAD)

--- Autobio Game/Assets/CameraControl.cs
+++ Autobio Game/Assets/CameraControl.cs
@@ -11,24 +11,106 @@ public class CameraControl : MonoBehaviour
     public float pitch; // the mouse movement up/down
     public float yaw;   // the mouse movement left/right
 
+    public GameObject gumband;
+    public Transform gumbandTran;
 
+    public GameObject lamp;
+    public Transform lampT;
+
+    public GameObject book;
+
+    public bool start;
+    public float lerpTime = 0;
+
+    public Transform currentTransform;
+    public Quaternion from;
+    public Quaternion to;
     public Vector3 inputVelocity;  // cumulative velocity to move character
 
     public float velocityModifier;  // velocity of conroller multiplied by this number
+    public float speed = 0.1f;
+
+    public GameObject player;
+    public sphereMovement myScript;
+
+    public GameObject curr;
 
     void Start()
     {
         thisRigidBody = GetComponent<Rigidbody>();
+        gumbandTran = gumband.GetComponent<Transform>();
+        lampT = lamp.GetComponent<Transform>();
+        //myScript = player.GetComponent<sphereMovement>();
     }
 
     void Update()
     {
 
+        /*
         yaw = Input.GetAxis("Mouse X");
         transform.Rotate(0, yaw, 0);
 
         pitch = Input.GetAxis("Mouse Y");
         thisCamera.transform.Rotate(-pitch, 0, 0);
+        */
+        /*
+        if (Input.GetKey(KeyCode.Space))
+        {
+            thisCamera.transform.LookAt(gumbandTran);
+        }else if (Input.GetKey(KeyCode.Z))
+        {
+            thisCamera.transform.LookAt(sphereT);
+        }
+        else
+        {
+            thisCamera.transform.LookAt(Vector3.zero);
+        }
+        */
+
+        if (Input.GetKey(KeyCode.Keypad1))
+        {
+            if (lamp.transform != currentTransform)
+            {
+                startLerp(lamp);
+            }
+        }
+        else if (Input.GetKey(KeyCode.Keypad2)) {
+            if (gumband.transform != currentTransform)
+            {
+                startLerp(gumband);
+            }
+        }
+        else if (Input.GetKey(KeyCode.Keypad3)) {
+            if (book.transform != currentTransform)
+            {
+                startLerp(book);
+            }
+        }
+        else
+        {
+            curr = myScript.getCurrObject();
+            startLerp(curr);
+        }
+
+        if (start)
+        {
+            lerpTime += Time.deltaTime;
+            Quaternion newRot = Quaternion.Lerp(from, Quaternion.LookRotation((transform.position - currentTransform.position)),  lerpTime * speed);
+            transform.rotation = newRot;
+            if (transform.rotation == Quaternion.LookRotation((transform.position - currentTransform.position)))
+            {
+                start = false;
+                lerpTime = 0;
+            }
+        }
+    }
+
+    public void startLerp(GameObject g)
+    {  
+        from = transform.rotation;
+        lerpTime = 0;
+        start = true;
+        currentTransform = g.GetComponent<Transform>();
 
     }
 }
--- Autobio Game/Assets/sphereMovement.cs
+++ Autobio Game/Assets/sphereMovement.cs
@@ -9,14 +9,47 @@ public class sphereMovement : MonoBehaviour
     public float moveVelocity;
     public float forceMultiplier;
 
-    void Start()
-    {
-        
-    }
+    public GameObject table;
 
+    public GameObject currObject;
+
+    private void Start()
+    {
+        currObject = table;
+        thisRigidBody = GetComponent<Rigidbody>();
+
+    }
 
     void Update()
     {
-        
+        inputAxis.y = Input.GetAxis("Vertical");
+        inputAxis.x = Input.GetAxis("Horizontal");
+    }
+
+
+    void FixedUpdate()
+    {
+        moveVelocity = thisRigidBody.velocity.magnitude;
+
+        if (thisRigidBody.velocity.magnitude < 8f)
+        {
+            thisRigidBody.AddForce(transform.up * inputAxis.y * forceMultiplier, ForceMode.Impulse);
+            thisRigidBody.AddForce(transform.right * inputAxis.x * forceMultiplier, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "interactive" )
+        {
+            //if (Input.GetKey(KeyCode.Space)) {
+                currObject = col.gameObject;
+            //}
+        }
+    }
+
+    public GameObject getCurrObject()
+    {
+        return currObject;
     }
 }
